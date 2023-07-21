738d2791d0b57dd7cbb66b513ed51167769d2f6f
gramos0211 <45494272+gramos0211@users.noreply.github.com>
2023-07-20 23:37:15 -0400

    init commit

---
+++ routes/record.js
@@ -0,0 +1,65 @@
+import express from "express";
+import {records_db as db} from "../db/conn.js";
+import { ObjectId } from "mongodb";
+
+const router = express.Router();
+
+// get list of all records
+router.get("/", async (req, res) => {
+    let collection = await db.collection("records");
+    let results = await collection.find({}).toArray();
+    res.send(results).status(200);
+})
+
+// get single record by id
+router.get("/:id", async (req, res) => {
+    let collection = await db.collection("records");
+    let query = {_id: new ObjectId(req.params.id)};
+    let result = await collection.findOne(query);
+
+    if (!result) res.send("Not found").status(404)
+    else res.send(result).status(200);
+})
+
+// create a new record
+router.post("/", async (req, res) => {
+    let newDocument = {
+        name: req.body.name,
+        position: req.body.position,
+        level: req.body.level,
+    };
+    
+    let collection = await db.collection("records");
+    let result = await collection.insertOne(newDocument);
+    
+    res.send(result).status(204);
+})
+
+// update a record by id
+router.patch("/:id", async (req, res) => {
+    const query = {_id: new ObjectId(req.params.id)}
+    const updates = {
+        $set: {
+            name: req.body.name,
+            position: req.body.position,
+            level: req.body.level,
+        }
+    };
+
+    let collection = await db.collection("records")
+    let result = collection.updateOne(query, updates)
+
+    res.send(result).status(200)
+});
+
+// delete a record by id
+router.delete("/:id", async (req, res) => {
+    const query = { _id: new ObjectId(req.params.id) };
+
+    const collection = db.collection("records");
+    let result = await collection.deleteOne(query);
+
+    res.send(result).status(200);
+})
+
+export default router;
---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import records from "./routes/record.js";
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/record", records)
+
+// start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+})
